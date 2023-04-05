78bf80893f1a6aba976ceeaacecda926bc0c0408
Shashika Rameesha Jayawardena <shashikajayawardena2001@gmail.com>
2023-04-05 14:44:16 +0530

    created user.java

---
+++ src/lk/ijse/gdse/hostelManagement/entity/User.java
@@ -0,0 +1,40 @@
+package lk.ijse.gdse.hostelManagement.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @Column(name = "userName",length = 25)
+    private String userName;
+    @Column(name = "password")
+    private String Password;
+
+    public User() {
+    }
+
+    public User(String userName, String password) {
+        this.userName = userName;
+        Password = password;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String password) {
+        Password = password;
+    }
+}
