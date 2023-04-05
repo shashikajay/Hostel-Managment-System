e235e2b71bc4294d40802313f8a606badcc21394
Shashika Rameesha Jayawardena <shashikajayawardena2001@gmail.com>
2023-04-05 14:17:11 +0530

    create hibernate.propertie

--- src/lk/ijse/gdse/hostelManagement/AppInitializer.java
+++ src/lk/ijse/gdse/hostelManagement/AppInitializer.java
@@ -2,20 +2,22 @@ package lk.ijse.gdse.hostelManagement;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
 
 public class AppInitializer extends Application {
-
     @Override
-    public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("lk/ijse/gdse/hostelManagement/view/LoginForm.fxml"));
-        primaryStage.setScene(new Scene(root));
+    public void start(Stage primaryStage) throws IOException {
+        primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("view/LoginForm.fxml"))));
+        primaryStage.initStyle(StageStyle.UNDECORATED);
+        primaryStage.centerOnScreen();
+        primaryStage.setResizable(false);
         primaryStage.show();
     }
-
     public static void main(String[] args) {
-        launch(args);
+        launch (args);
     }
 }
