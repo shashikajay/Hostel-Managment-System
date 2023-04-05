d7bb433ed782bd84fb5d9db480a9408618dabf5e
Shashika Rameesha Jayawardena <shashikajayawardena2001@gmail.com>
2023-04-05 13:56:21 +0530

    Update Appinizializer.java

--- src/lk/ijse/gdse/hostelManagement/AppInitializer.java
+++ src/lk/ijse/gdse/hostelManagement/AppInitializer.java
@@ -1,4 +1,21 @@
 package lk.ijse.gdse.hostelManagement;
 
-public class AppInitializer {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class AppInitializer extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("lk/ijse/gdse/hostelManagement/view/LoginForm.fxml"));
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
